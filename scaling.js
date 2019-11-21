c762476aa2a9a7835c41721c74622f5aa717d99b
Robert Bacon <rb40345@gmail.com>
2019-11-21 08:19:02 -0600

    changed to cleaner approach

--- scaling.js
+++ scaling.js
@@ -3,7 +3,13 @@ const distance=(x1, y1, x2, y2)=>{
     return Math.sqrt(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2))
 }
 const angle=(centerX, centerY, x2, y2, x3, y3)=>{
-    return (((x2-centerX)*(x3-centerX))+((y2-centerY)*(y3-centerY)))/(distance(centerX, centerY, x2, y2)*distance(centerX, centerY, x3, y3));
+    //Need to include a check for sign of the angle
+    return Math.acos((((x2-centerX)*(x3-centerX))+((y2-centerY)*(y3-centerY)))/(distance(centerX, centerY, x2, y2)*distance(centerX, centerY, x3, y3)));
+}
+
+const nextPoint=(x1, y1, x2, y2, theta, distance)=>{
+    let temp=angle(x1, y1, x2, y2, x2, y1)+theta;
+    return {x: x1+distance*Math.cos(temp), y: y1+distance*Math.sin(temp)}
 }
 
 const dDistancedX1=(x1, y1, x2, y2)=>{
@@ -24,19 +30,35 @@ const dAngledx2=(centerX, centerY, x2, y2, x3, y3)=>{
     return (((u*dv)-(v*du))/(Math.pow(v, 2)))
 
 }
+/*
+    We have 16 nodes
+    Hence, 32 values we're optimizing over
+    We
 
+*/
 const FArr=(vec)=>{
     //Do some stuff in a bit
 }
 const Jacobian=(vec)=>{
     //Do some stuff in a bit
 }
+const vecToFrame=(vec)=>{
+    //Do some stuff
+}
 
 const scale=(targets, errBound)=>{
 const targetParams=[];
 let currVals=[];
-let error=[]//mathJS.add(targetParams, mathJS.multiply(currVals-1));
-while(mathJS.size(error)>errBound){
-
+let currFArr=FArr(currVals)
+let error=(mathJS.add(targetParams, mathJS.multiply(currFArr,-1)));
+while(mathJS.dot(error, error)>errBound){
+    currVals=mathJS.add(currVals, mathJS.multiply(mathJS.multiply(mathJS.inv(Jacobian(currVals)), currFArr), -1));
+    currFArr=FArr(currVals);
 }
+return currVals;
+}
+
+
+module.exports={
+    scale, angle, distance
 }
