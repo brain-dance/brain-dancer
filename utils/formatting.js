aede5e68868ce10a24c19c4d044042c1a4e64eca
Dante Tobar <dante.m.tobar@gmail.com>
2019-11-22 14:01:15 -0600

    moved net creator out of file

--- utils/formatting.js
+++ utils/formatting.js
@@ -1,36 +1,6 @@
-const posenet = require('@tensorflow-models/posenet');
 const {angle} = require('./scaling');
 const {createCanvas, loadImage} = require('canvas');
-
-const singlePoseNet = () => {
-  let poseNetConfig = {
-    algorithm: 'single-pose', //other option: multi-pose
-    input: {
-      architecture: 'MobileNetV1',
-      outputStride: 16,
-      inputResolution: {width: 640, height: 360},
-      multiplier: 0.75,
-      quantBytes: 2
-    },
-    singlePoseDetection: {
-      minPoseConfidence: 0.1,
-      minPartConfidence: 0.5
-    },
-    output: {
-      showVideo: true,
-      showPoints: true
-    }
-  };
-  return posenet
-    .load({
-      architecture: poseNetConfig.input.architecture,
-      outputStride: poseNetConfig.input.outputStride,
-      inputResolution: poseNetConfig.input.inputResolution,
-      multiplier: poseNetConfig.input.multiplier,
-      quantBytes: poseNetConfig.input.quantBytes
-    })
-    .then(net => net);
-};
+const {singlePoseNet} = require('./posenet');
 
 const getPose = async input => {
   const net = await singlePoseNet();
