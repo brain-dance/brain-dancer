85e0431c574f62ed3615ac07d9c3cc354cd0f876
Jennie-Myhren <jenniewalters2012@u.northwestern.edu>
2019-11-23 17:21:59 -0600

    Dashboard sidebar displays team

---
+++ client/components/Dash_Sidebar.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+// import {useDispatch, useSelector} from 'react-redux';
+import {Header, Segment, Grid} from 'semantic-ui-react';
+import TeamList from './TeamList';
+// import {getUser} from '../store/user';
+
+export const DashSidebar = props => {
+  const {user, selectedTeam, handleSelectTeam} = props;
+  const {teams} = user;
+
+  // const [isClickedAddTeam, setIsClickedAddTeam] = useState(false);
+
+  // const handleClickAddTeam = () => {
+  //   setIsClickedAddTeam(true);
+  // };
+
+  return user ? (
+    <div>
+      <Header as="h3">Assignments</Header>
+      <Grid>
+        <Grid.Column width={3}>
+          <Header as="h3">Teams</Header>
+        </Grid.Column>
+        <Grid.Column width={1}>
+          {/* BUTTON MIGHT MOVE TO ADD TEAM MODAL */}
+          {/* <Button size="mini" color="olive" icon onClick={handleClickAddTeam}>
+            <Icon name="add square" />
+          </Button> */}
+        </Grid.Column>
+      </Grid>
+      <TeamList
+        teams={teams}
+        selectedTeam={selectedTeam}
+        handleSelectTeam={handleSelectTeam}
+      />
+    </div>
+  ) : (
+    <h3>Error!</h3>
+  );
+};
+
+export default DashSidebar;
