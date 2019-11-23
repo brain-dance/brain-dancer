74a9514a7f8641ed3b4f0bce07cea5ee709fff4b
Jasmine Wang <jasminey.tea@gmail.com>
2019-11-23 11:21:46 -0600

    added team reducer and included in combineReducers

---
+++ client/store/team.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+// ACTION CONSTANTS
+
+// ACTION CREATORS
+
+// THUNKS
+
+// INITIAL STATE
+
+// REDUCER
+const reducer = (state, actions) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+// EXPORT
+export default reducer;
