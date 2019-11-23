1fecb021b1233e384037a1f4a815644989daf166
Jasmine Wang <jasminey.tea@gmail.com>
2019-11-23 10:50:34 -0600

    fixed component linking error and added semantic ui react to package.json

--- client/components/index.js
+++ client/components/index.js
@@ -1,3 +1,3 @@
 export {default as Navbar} from './Navbar';
-export {default as UserHome} from './User-Home';
+export {default as UserHome} from './UserHome';
 export {Login, Signup} from './Auth-Form';
