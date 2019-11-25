46b496f9f59893fa83cf60e88d89b94338bf65ac
Dante Tobar <dante.m.tobar@gmail.com>
2019-11-25 08:20:25 -0600

    added routine title

--- client/components/Routines.js
+++ client/components/Routines.js
@@ -4,9 +4,14 @@ import {Routine} from './index';
 const Routines = props => {
   const routines = props.routines || [];
 
-  return routines.map(routine => (
-    <Routine key={routine.id} routine={routine} />
-  ));
+  return (
+    <div id="routines">
+      <h1>Routines</h1>
+      {routines.map(routine => (
+        <Routine key={routine.id} routine={routine} />
+      ))}
+    </div>
+  );
 };
 
 export default Routines;
