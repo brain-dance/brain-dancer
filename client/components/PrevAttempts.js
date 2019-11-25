6d3dd7a9c77b2a31b582845512981bb7abb7debe
Jennie-Myhren <jenniewalters2012@u.northwestern.edu>
2019-11-25 13:36:09 -0600

    horizontal scroll works; refactoring now

---
+++ client/components/PrevAttempts.js
@@ -0,0 +1,105 @@
+import React, {Component} from 'react';
+import ScrollMenu from 'react-horizontal-scrolling-menu';
+
+// import {Header, Segment, Grid} from 'semantic-ui-react';
+
+const MenuItem = ({text, selected}) => {
+  return <div className={`menu-item ${selected ? 'active' : ''}`}>{text}</div>;
+};
+
+export const Menu = (list, selected) =>
+  list.map(el => {
+    const {name} = el;
+
+    return <MenuItem text={name} key={name} selected={selected} />;
+  });
+
+const Arrow = ({text, className}) => {
+  return <div className={className}>{text}</div>;
+};
+
+const ArrowLeft = Arrow({text: '<', className: 'arrow-prev'});
+const ArrowRight = Arrow({text: '>', className: 'arrow-next'});
+
+const MySelected = 'video1';
+
+// const PrevAttempts = () => {
+const recorded = [
+  {name: 'video1'},
+  {name: 'video2'},
+  {name: 'video3'},
+  {name: 'video3'},
+  {name: 'video3'},
+  {name: 'video3'},
+  {name: 'video3'},
+  {name: 'video3'},
+  {name: 'video3'},
+  {name: 'video3'},
+  {name: 'video3'},
+  {name: 'video3'},
+  {name: 'video3'},
+  {name: 'video3'},
+  {name: 'video1'},
+  {name: 'video2'},
+  {name: 'video3'},
+  {name: 'video3'},
+  {name: 'video3'},
+  {name: 'video3'},
+  {name: 'video3'},
+  {name: 'video3'},
+  {name: 'video3'},
+  {name: 'video3'},
+  {name: 'video3'},
+  {name: 'video3'},
+  {name: 'video3'},
+  {name: 'video3'},
+  {name: 'video1'},
+  {name: 'video2'},
+  {name: 'video3'},
+  {name: 'video3'},
+  {name: 'video3'},
+  {name: 'video3'},
+  {name: 'video3'},
+  {name: 'video3'},
+  {name: 'video3'},
+  {name: 'video3'},
+  {name: 'video3'},
+  {name: 'video3'},
+  {name: 'video3'},
+  {name: 'video3'}
+];
+
+// return (
+const PrevAttempts = () => {
+  // constructor(props) {
+  //   super(props);
+  // call it again if items count changes
+  const menuItems = Menu(recorded, MySelected);
+
+  const state = {
+    selected
+  };
+
+  // const onSelect = key => {
+  //   setState({selected: key});
+  // };
+
+  // render() {
+  const {selected} = state;
+  // Create menu from items
+  const menu = menuItems;
+
+  return (
+    <div className="App">
+      <ScrollMenu
+        data={menu}
+        arrowLeft={ArrowLeft}
+        arrowRight={ArrowRight}
+        selected={selected}
+        // onSelect={onSelect}
+      />
+    </div>
+  );
+};
+
+export default PrevAttempts;
