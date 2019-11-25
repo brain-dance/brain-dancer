8d3a872612b8ac00aed10180c8e103046d0164c0
Jasmine Wang <jasminey.tea@gmail.com>
2019-11-25 13:44:53 -0600

    cleaner menu with page title as part of title bar

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -3,42 +3,42 @@ import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 import {logout} from '../store';
-import {Button, Menu} from 'semantic-ui-react';
+import {Container, Header, Menu} from 'semantic-ui-react';
 
 const Navbar = ({handleClick, isLoggedIn}, props) => (
   <div>
-    <h1>BrainDancer</h1>
-    <nav>
-      {isLoggedIn ? (
-        <Menu>
-          {/* The Menubar will show these links after you log in */}
-          <Menu.Item name="home" as={Link} to="/home">
-            Home
-          </Menu.Item>
+    {isLoggedIn ? (
+      <Menu secondary>
+        {/* The Menubar will show these links after you log in */}
+        <Menu.Item name="home" as={Link} to="/home">
+          <Header as="h1">BrainDancer</Header>
+        </Menu.Item>
 
-          <Menu.Item name="logout" onClick={handleClick}>
-            Logout
-          </Menu.Item>
-        </Menu>
-      ) : (
-        <Menu>
-          {/* The navbar will show these links before you log in */}
-          <Menu.Item name="login" as={Link} to="/login">
-            Login
-          </Menu.Item>
-          <Menu.Item name="signup" as={Link} to="/signup">
-            Sign Up
-          </Menu.Item>
-        </Menu>
-      )}
-    </nav>
-    <hr />
+        <Menu.Item name="home" as={Link} to="/home">
+          Home
+        </Menu.Item>
+
+        <Menu.Item name="logout" onClick={handleClick}>
+          Logout
+        </Menu.Item>
+      </Menu>
+    ) : (
+      <Menu secondary>
+        {/* The navbar will show these links before you log in */}
+        <Menu.Item name="login" as={Link} to="/login">
+          <Header as="h1">BrainDancer</Header>
+        </Menu.Item>
+        <Menu.Item name="login" as={Link} to="/login">
+          Login
+        </Menu.Item>
+        <Menu.Item name="signup" as={Link} to="/signup">
+          Sign Up
+        </Menu.Item>
+      </Menu>
+    )}
   </div>
 );
 
-/**
- * CONTAINER
- */
 const mapState = state => {
   return {
     isLoggedIn: !!state.user.id
