422d2fe3b479195d6e43621f1fce917b7728cb59
Jasmine Wang <jasminey.tea@gmail.com>
2019-11-25 13:34:09 -0600

    more stylin

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -5,32 +5,31 @@ import {Link} from 'react-router-dom';
 import {logout} from '../store';
 import {Button, Menu} from 'semantic-ui-react';
 
-const Navbar = ({handleClick, isLoggedIn}) => (
+const Navbar = ({handleClick, isLoggedIn}, props) => (
   <div>
     <h1>BrainDancer</h1>
-    <Menu>
+    <nav>
       {isLoggedIn ? (
-        <div>
+        <Menu>
           {/* The Menubar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
+          <Menu.Item name="home" component={Link} to="/home">
+            Home
+          </Menu.Item>
 
-        <Menu.Item
-        name="login"
-        onClick={}
-        >Login
+          <Menu.Item name="logout" onClick={handleClick}>
+            Logout
           </Menu.Item>
-        <Menu.Item>Sign Up
+        </Menu>
+      ) : (
+        <Menu>
+          {/* The navbar will show these links before you log in */}
+          <Menu.Item name="login" component={Link} to="/login">
+            Login
           </Menu.Item>
+          <Menu.Item name="signup" component={Link} to="/signup">
+            Sign Up
+          </Menu.Item>
+        </Menu>
       )}
     </nav>
     <hr />
