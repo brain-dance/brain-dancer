e022397077fb52696651372428170dc15926e575
Jasmine Wang <jasminey.tea@gmail.com>
2019-11-26 10:47:21 -0600

    added logic to link to routines properly

--- client/components/Routine.js
+++ client/components/Routine.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import {withRouter} from 'react-router-dom';
 
+import {Link} from 'react-router-dom';
+
 const Routine = props => {
   const {routine} = props;
+  console.log('routine', props);
+  const teamId = props.match.params.teamId || routine.teamId;
 
-  const handleClick = id => {
-    props.history.push(`/routines/${id}`);
-  };
+  // const handleClick = id => {
+  //   props.history.push(`/routines/${id}`);
+  // };
 
   return (
-    <div className="routine-card" onClick={() => handleClick(routine.id)}>
-      <p>{routine.title}</p>
-    </div>
+    // <div className="routine-card" onClick={() => handleClick(routine.id)}>
+    <Link to={`/team/${teamId}/routine/${routine.id}`}>
+      <div className="routine-card">
+        <p>{routine.title}</p>
+      </div>
+    </Link>
   );
 };
 
