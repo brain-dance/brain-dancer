1bbd3244fd3b44cde7cc2f566b0979e1e304d999
Robert Bacon <rb40345@gmail.com>
2019-11-26 14:55:53 -0600

    Video playing should in principle now work

--- client/routes.js
+++ client/routes.js
@@ -11,6 +11,7 @@ import {
   RecordRoutine,
   RecordPractice
 } from './components';
+import WatchVideo from './components/watchvideo'
 import {me} from './store';
 
 /**
@@ -35,6 +36,7 @@ class Routes extends Component {
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
+            <Route path="/watch/routines/:id" component={WatchVideo}/>
             <Route exact path="/home" component={Dashboard} />
             <Route path="/team/:teamId/add" component={RecordRoutine} />
             <Route path="/team/:teamId/routine/:routineId" component={Choreo} />
