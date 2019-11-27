c06147d955da8db717bae165ae02a9c82e397bc5
Dante Tobar <dante.m.tobar@gmail.com>
2019-11-27 13:30:50 -0600

    export set routine action creator

--- client/store/singleRoutine.js
+++ client/store/singleRoutine.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
 
 // action constants
-export const SET_SINGLE_ROUTINE = 'SET_SINGLE_ROUTINE';
+const SET_SINGLE_ROUTINE = 'SET_SINGLE_ROUTINE';
 
 // action creators
-const setSingleRoutine = routine => ({type: SET_SINGLE_ROUTINE, routine});
+export const setSingleRoutine = routine => ({
+  type: SET_SINGLE_ROUTINE,
+  routine
+});
 
 // thunks
 export const getSingleRoutine = routineId => async dispatch => {
