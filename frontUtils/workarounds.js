7991d510fe64b54e264fc9b666f95531e88fd667
Dante Tobar <dante.m.tobar@gmail.com>
2019-12-01 17:21:00 -0600

    webcam helper added

---
+++ frontUtils/workarounds.js
@@ -0,0 +1,11 @@
+export const stopWebcam = videoNode => {
+  const stream = videoNode.srcObject;
+  const tracks = stream.getTracks();
+
+  for (let i = 0; i < tracks.length; i++) {
+    const track = tracks[i];
+    track.stop();
+  }
+
+  videoNode.srcObject = null;
+};
