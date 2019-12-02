70ebe996a03c71ff02d5c7b204bf937565e4320d
Jennie-Myhren <jenniewalters2012@u.northwestern.edu>
2019-12-01 18:33:09 -0600

    placeholder for no assignments on /my-assignments

--- client/components/Assignments.js
+++ client/components/Assignments.js
@@ -1,29 +1,46 @@
-import React, {useState} from 'react';
-import {Image, Label, Menu} from 'semantic-ui-react';
+import React, {useState, useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {withRouter} from 'react-router-dom';
+import {Button, Header, Image, Item, Label, Segment} from 'semantic-ui-react';
+import {fetchAssignments} from '../store/assignment';
 
-export const AssignmentList = props => {
-  const {assignments} = props;
+export const Assignments = props => {
+  const assignments = useSelector(state => state.assignment);
+  //Q. DO WE WANT ASSIGNMENTS AS AN OBJECT??
   console.log('TCL: assignments', assignments);
   const [isActiveItem, setIsActiveItem] = useState('');
-
+  const dispatch = useDispatch();
   const handleItemClick = (e, {name}) => {
     setIsActiveItem(name);
   };
 
+  useEffect(() => {
+    dispatch(fetchAssignments());
+  });
+
+  const redirectToDashboard = () => {
+    props.history.push('/dashboard');
+  };
+
   return !assignments.length ? (
-    <div>
-      <br />
-      <Image
-        size="mini"
-        src="https://static.thenounproject.com/png/26965-200.png"
-      />
-      <p>Your tasks are done. Go you!</p>
-    </div>
+    <Segment placeholder color="orange">
+      <Header icon>
+        Your tasks are done. Go you!{' '}
+        <Image
+          size="big"
+          src="https://static.thenounproject.com/png/26965-200.png"
+        />
+      </Header>
+      <Button primary onClick={redirectToDashboard}>
+        Back to Dashboard
+      </Button>
+    </Segment>
   ) : (
-    <Menu pointing vertical>
+    // <Menu pointing vertical>
+    <div>
       {assignments.map(assignment => {
         return (
-          <Menu.Item
+          <Item
             key={assignment.id}
             name={assignment.id}
             active={isActiveItem === name}
@@ -40,11 +57,12 @@ export const AssignmentList = props => {
             ) : (
               <Label color="pink" />
             )}
-          </Menu.Item>
+          </Item>
         );
       })}
-    </Menu>
+    </div>
+    // </Menu>
   );
 };
 
-export default AssignmentList;
+export default withRouter(Assignments);
