0b12a1515971fa526bd1dadaad14d603a9b95b23
Dante Tobar <dante.m.tobar@gmail.com>
2019-12-02 18:13:32 -0600

    added assignment api routes

--- server/api/assignments.js
+++ server/api/assignments.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const {Assignment, Routine, Team} = require('../db/models');
+const {Assignment, Routine, Team, User} = require('../db/models');
 module.exports = router;
 
 router.get('/', async (req, res, next) => {
@@ -13,6 +13,40 @@ router.get('/', async (req, res, next) => {
       });
       res.send(userAssignments);
     }
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const {userId, routineId} = req.body;
+    const temp = await Assignment.create({
+      userId,
+      routineId,
+      completed: false
+    });
+    const assignment = await Assignment.findByPk(temp.id, {
+      include: {model: User}
+    });
+    res.send(assignment);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    await Assignment.update(
+      {completed: true},
+      {
+        where: {
+          userId: req.user.id,
+          routineId: req.params.id
+        }
+      }
+    );
+    res.status(201).end();
   } catch (err) {
     next(err);
   }
