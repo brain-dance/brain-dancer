81b226658279166b435a2590932d3fa18f21e7d7
Dante Tobar <dante.m.tobar@gmail.com>
2019-12-02 18:12:21 -0600

    added post assignment thunk

--- client/store/singleRoutine.js
+++ client/store/singleRoutine.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+import ADD_ASSIGNMENT from './assignment';
+
 // action constants
 const SET_SINGLE_ROUTINE = 'SET_SINGLE_ROUTINE';
 
@@ -27,6 +29,9 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_SINGLE_ROUTINE:
       return action.routine;
+    case ADD_ASSIGNMENT:
+      state.assignments.push(action.assignment);
+      return state;
     default:
       return state;
   }
