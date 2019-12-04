7e7b46a1139aae77ffc495e14b4a3f141502436a
Jennie-Myhren <jenniewalters2012@u.northwestern.edu>
2019-12-04 10:48:55 -0600

    PrevAttempts play, pause + card = button

--- client/components/PrevAttempts.js
+++ client/components/PrevAttempts.js
@@ -4,14 +4,21 @@ import UploadVideoForm from './UploadVideoForm';
 
 const PrevAttempts = props => {
   let {recording, handleDelete, teamId, userId} = props;
-  console.log('TCL: recording', recording);
-
   const [isSelected, setIsSelected] = useState({});
-  let attempt = 0;
 
   const handleSelect = (e, {name}) => {
     let selectedBlob = recording.find(blob => blob.name === name); //video = blob
     setIsSelected(selectedBlob);
+  };
+
+  const handlePlay = (e, {name}) => {
+    let selectedVid = document.getElementById(name); //video = blob
+    selectedVid.play();
+  };
+
+  const handlePause = (e, {name}) => {
+    let selectedVid = document.getElementById(name); //video = blob
+    selectedVid.pause();
   };
 
   return !recording.length ? (
@@ -21,42 +28,29 @@ const PrevAttempts = props => {
       <Header as="h3">Recorded Attempts</Header>
       {recording.map(blob => {
         let blobSrc = URL.createObjectURL(blob);
-        let selected = isSelected.name === blob.name;
-        attempt++;
 
         return (
-          <div id="previous-attempt" key={blob.name}>
-            <Card>
-              <Button.Group vertical>
-                {/* This button should be smaller; fix later */}
-                <Button
-                  icon
-                  circular
-                  name={blob.name}
-                  onClick={handleDelete}
-                  color="black"
-                  size="mini"
-                  floated="right"
-                >
+          <div className="vidCard" id="previous-attempt" key={blob.name}>
+            <Card name={blob.name} onClick={handleSelect}>
+              <video id={blob.name} width="200" src={blobSrc} />
+              <Button.Group>
+                <Button name={blob.name} icon onClick={handlePlay}>
+                  <Icon name="play" />
+                </Button>
+                <Button name={blob.name} icon onClick={handlePause}>
+                  <Icon name="pause" />
+                </Button>
+                <Button icon name={blob.name} onClick={handleDelete}>
                   <Icon name="delete" />
                 </Button>
-                <Button
-                  color={selected ? 'blue' : 'black'}
-                  type="button"
-                  key={blob.name}
-                  name={blob.name}
-                  onClick={handleSelect}
-                >
-                  <video controls id={blob.name} width="200" src={blobSrc} />
-                </Button>
-                <UploadVideoForm
-                  attached="bottom"
-                  blob={blob}
-                  teamId={teamId}
-                  userId={userId}
-                  calibration={props.calibration}
-                />
               </Button.Group>
+              <UploadVideoForm
+                attached="bottom"
+                blob={blob}
+                teamId={teamId}
+                userId={userId}
+                calibration={props.calibration}
+              />
             </Card>
           </div>
         );
