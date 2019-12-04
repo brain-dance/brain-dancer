b7ad9bde2b9696e8bba9f974fc42cbb9bb3c0be0
Jennie-Myhren <jenniewalters2012@u.northwestern.edu>
2019-12-04 13:40:41 -0600

    MembersSidebar appears in Team component

---
+++ client/components/MembersSidebar.js
@@ -0,0 +1,77 @@
+import React, {useState} from 'react';
+import {withRouter} from 'react-router-dom';
+import {Member} from './index';
+import {Menu, Sidebar} from 'semantic-ui-react';
+
+const MembersSidebar = props => {
+  const members = props.members || [];
+  const [visible, setVisible] = useState(true);
+
+  //NEED WAY TO HANDLE OPEN
+  //NEED TO MOVE THIS
+  const handleOpen = () => {
+    setVisible(true);
+  };
+
+  const handleHide = () => {
+    setVisible(false);
+  };
+
+  return (
+    <Sidebar
+      as={Menu}
+      animation="overlay"
+      icon="labeled"
+      direction="right"
+      onHide={handleHide}
+      vertical
+      visible={visible}
+      width="wide"
+    >
+      {/* <Menu.Item> */}
+      {members.map(member => (
+        <Member
+          member={member}
+          key={member.id}
+          handleUpdateTeam={props.handleUpdateTeam}
+        />
+      ))}
+      {/* </Menu.Item> */}
+      {/* <Menu.Item as="a">
+          <Icon name="home" />
+          Home
+        </Menu.Item>
+        <Menu.Item as="a">
+          <Icon name="gamepad" />
+          Games
+        </Menu.Item>
+        <Menu.Item as="a">
+          <Icon name="camera" />
+          Channels
+        </Menu.Item>*/}
+    </Sidebar>
+  );
+  //   <Sidebar.Pusher>
+  //     <Segment basic>
+  //       <Header as="h3">Members</Header>
+  //       <Image src="/images/wireframe/paragraph.png" />
+  //     </Segment>
+  //   </Sidebar.Pusher>
+  // </Sidebar.Pushable>
+  // );
+  // <Segment basic color="blue">
+  //   <Header as="h3">Members</Header>
+  //   <List divided verticalAlign="middle">
+  //     {members.map(member => (
+  //       <Member
+  //         member={member}
+  //         key={member.id}
+  //         handleUpdateTeam={props.handleUpdateTeam}
+  //       />
+  //     ))}
+  //   </List>
+  // </Segment>
+  // );
+};
+
+export default withRouter(MembersSidebar);
--- client/components/Team.js
+++ client/components/Team.js
@@ -1,38 +1,43 @@
 import React from 'react';
 import {withRouter, Link} from 'react-router-dom';
-import {Routines, Members} from './index';
-import {Segment, Header, Button, Icon} from 'semantic-ui-react';
+import {Routines, MembersSidebar} from './index';
+import {Segment, Header, Button, Icon, Sidebar} from 'semantic-ui-react';
+// import MembersSidebar from './MembersSidebar';
+// import {TeamList} from './TeamList';
 
 const Team = props => {
   const team = props.team;
 
   const myRole = team.role;
   return (
-    <Segment raised id="team">
-      <Header as="h1">
-        {team.name}{' '}
-        {myRole === 'choreographer' && (
-          <Button.Group floated="right">
-            <Button
-              color="orange"
-              onClick={() => props.setMemberModalOpen(true)}
-            >
-              <Icon name="user plus" />
-            </Button>
-            <Button primary as={Link} to={`/team/${props.team.id}/add`}>
-              <Icon name="add" />
-              <Icon name="record" />
-            </Button>
-          </Button.Group>
-        )}
-      </Header>
-
-      <Routines team={team} routines={team.routines} />
-      <Members
+    <Sidebar.Pushable as={Segment}>
+      <MembersSidebar
         members={team.members}
         handleUpdateTeam={props.handleUpdateTeam}
       />
-    </Segment>
+      <Sidebar.Pusher>
+        <Segment raised id="team">
+          <Header as="h1">
+            {team.name}{' '}
+            {myRole === 'choreographer' && (
+              <Button.Group floated="right">
+                <Button
+                  color="orange"
+                  onClick={() => props.setMemberModalOpen(true)}
+                >
+                  <Icon name="user plus" />
+                </Button>
+                <Button primary as={Link} to={`/team/${props.team.id}/add`}>
+                  <Icon name="add" />
+                  <Icon name="record" />
+                </Button>
+              </Button.Group>
+            )}
+          </Header>
+          <Routines team={team} routines={team.routines} />
+        </Segment>
+      </Sidebar.Pusher>
+    </Sidebar.Pushable>
   );
 };
 
