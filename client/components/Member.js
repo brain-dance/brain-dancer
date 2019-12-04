99390dfa066e5f694c419bef5916610c8b9899b9
Jennie-Myhren <jenniewalters2012@u.northwestern.edu>
2019-12-04 13:40:53 -0600

    WIP rendering member with role

--- client/components/Member.js
+++ client/components/Member.js
@@ -1,39 +1,59 @@
 import React from 'react';
-import {useSelector, useDispatch} from 'react-redux';
+import {useSelector} from 'react-redux';
 import {withRouter} from 'react-router-dom';
-import {List, Icon} from 'semantic-ui-react';
+import {Icon, Menu} from 'semantic-ui-react';
 // import {deleteTeamMemberThunk} from '../store';
 
 const Member = props => {
   const {member} = props;
 
-  // const dispatch = useDispatch();
-
   //team info for checking if user is choreographer in this team
   const teamId = +props.match.params.teamId;
   const teams = useSelector(state => state.teams);
   const thisTeam = teams.find(team => team.id === teamId);
-  return (
-    <List.Item>
-      {thisTeam && thisTeam.role === 'choreographer' && (
-        <List.Content floated="right">
-          <Icon
-            name="trash"
-            onClick={() => props.handleUpdateTeam(teamId, member.id)}
-          />
-        </List.Content>
-      )}
-      {member.userteams.role === 'choreographer' ? (
-        <List.Content>
-          <Icon name="star" /> {member.name}
-        </List.Content>
-      ) : (
-        <List.Content>
-          <Icon name="male" /> {member.name}
-        </List.Content>
-      )}
-    </List.Item>
+  const isChoreo = member.userteams.role === 'choreographer';
+
+  console.log('TCL: member.userteams.role', member.userteams.role);
+
+  return isChoreo ? (
+    <Menu.Item>
+      <Menu.Content>
+        <Icon name="star" /> {member.name}
+      </Menu.Content>
+    </Menu.Item>
+  ) : (
+    <Menu.Item>
+      <Menu.Content>
+        <Icon name="male" /> {member.name}
+      </Menu.Content>
+    </Menu.Item>
   );
+  // </Menu.Item>
+  // );
+  // return (
+  // <Menu.Item>
+  //   {thisTeam && thisTeam.role === 'choreographer' && (
+  //     <Menu.Content floated="right">
+  //       <Icon
+  //         name="trash"
+  //         onClick={() => props.handleUpdateTeam(teamId, member.id)}
+  //       />
+  //     </Menu.Content>
+  //   )}
+  // <Menu.Item>
+  // <div>
+  //   {member.userteams.role === 'choreographer' ? (
+  //     <Menu.Content>
+  //       <Icon name="star" /> {member.name}
+  //     </Menu.Content>
+  //   ) : (
+  //     <Menu.Content>
+  //       <Icon name="male" /> {member.name}
+  //     </Menu.Content>
+  //   )}
+  // </div>
+  // {/* </Menu.Item> */}
+  // );
 };
 
 export default withRouter(Member);
