08cb7b5d0f6c1447255cfae5a27c4e256a97a2de
Jennie-Myhren <jenniewalters2012@u.northwestern.edu>
2019-12-04 18:44:01 -0600

    members sidebar opens/closes on btn click

--- client/components/MembersSidebar.js
+++ client/components/MembersSidebar.js
@@ -1,17 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {withRouter} from 'react-router-dom';
 import {Member} from './index';
 import {Menu, Sidebar} from 'semantic-ui-react';
 
 const MembersSidebar = props => {
   const members = props.members || [];
-  //THIS WILL DEFAULT TO NOT VISIBLE
-  const [visible, setVisible] = useState(true);
-
-  //NEED WAY TO HANDLE OPEN
-  const handleOpen = () => {
-    setVisible(true);
-  };
+  const {handleOpen, visible, setVisible} = props;
 
   const handleHide = () => {
     setVisible(false);
@@ -20,15 +14,16 @@ const MembersSidebar = props => {
   return (
     <Sidebar
       as={Menu}
+      inverted
       animation="overlay"
       icon="labeled"
       direction="right"
       onHide={handleHide}
+      onShow={handleOpen}
       vertical
       visible={visible}
-      width="wide"
+      width="thin"
     >
-      {/* <Menu.Item> */}
       {members.map(member => (
         <Member
           member={member}
@@ -36,42 +31,8 @@ const MembersSidebar = props => {
           handleUpdateTeam={props.handleUpdateTeam}
         />
       ))}
-      {/* </Menu.Item> */}
-      {/* <Menu.Item as="a">
-          <Icon name="home" />
-          Home
-        </Menu.Item>
-        <Menu.Item as="a">
-          <Icon name="gamepad" />
-          Games
-        </Menu.Item>
-        <Menu.Item as="a">
-          <Icon name="camera" />
-          Channels
-        </Menu.Item>*/}
     </Sidebar>
   );
-  //   <Sidebar.Pusher>
-  //     <Segment basic>
-  //       <Header as="h3">Members</Header>
-  //       <Image src="/images/wireframe/paragraph.png" />
-  //     </Segment>
-  //   </Sidebar.Pusher>
-  // </Sidebar.Pushable>
-  // );
-  // <Segment basic color="blue">
-  //   <Header as="h3">Members</Header>
-  //   <List divided verticalAlign="middle">
-  //     {members.map(member => (
-  //       <Member
-  //         member={member}
-  //         key={member.id}
-  //         handleUpdateTeam={props.handleUpdateTeam}
-  //       />
-  //     ))}
-  //   </List>
-  // </Segment>
-  // );
 };
 
 export default withRouter(MembersSidebar);
