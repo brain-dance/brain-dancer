06f61e71656bbc96de7b0bfac05be8ef1f3f3b56
Jasmine Wang <jasminey.tea@gmail.com>
2020-02-20 11:27:09 -0600

    inline docs for canvasify

--- backUtils/canvasify.js
+++ backUtils/canvasify.js
@@ -1,5 +1,9 @@
 const {singlePoseNet} = require('../utils/posenet');
 
+/* ********************
+  getPose takes input (image) and runs it through the PoseNet model
+  returns JSON object of 17 body key points
+   ******************** */
 const getPose = async input => {
   const net = await singlePoseNet();
   return net.estimateSinglePose(input, {
@@ -10,6 +14,12 @@ const getPose = async input => {
 const {createCanvas, loadImage} = require('canvas');
 const sizeOf = require('image-size');
 
+/* ********************
+  canvasify takes an image as input
+  creates a canvas and context, and draws it on the canvas.
+  This function is used in video processing on the back end, to generate a dance
+  routine's skellies to be saved to the database.
+   ******************** */
 const canvasify = async imagePath => {
   const dim = sizeOf(imagePath);
   const canvas = await createCanvas(dim.width, dim.height);
