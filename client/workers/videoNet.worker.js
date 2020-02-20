eba4644fb702f5e2fac093e944777420824772ee
Jasmine Wang <jasminey.tea@gmail.com>
2020-02-20 09:50:21 -0600

    added inline docs for webworker

--- client/workers/videoNet.worker.js
+++ client/workers/videoNet.worker.js
@@ -4,8 +4,16 @@ let poseNetConfig = {};
 let net = {};
 let calibration = {};
 
+/* ********************
+  When the webworker receives a message from RecordPractice, it is in an object
+  with at least one key, data, which has an object value equaling the
+  information sent from the React component.
+
+  On the first message received, the data should include the resolution of the
+  video recording. The webworker sets up poseNet config to have the appropriate
+  resolution and sends a 'Ready' message back.
+   ******************** */
 self.onmessage = set => {
-  console.log(set);
   if (set.data.resolution) {
     poseNetConfig = {
       algorithm: 'single-pose', //other option: multi-pose
@@ -26,15 +34,25 @@ self.onmessage = set => {
       }
     };
     postMessage({type: 'Ready'});
-    
-     
+
+    /* ********************
+      Outside of the initial resolution message, three types of messages
+        could be received:
+      'calibration' - a single image is sent to be processed through PoseNet,
+        to be stored and used to normalize dancer and choreographer skellies.
+      (no type) - data stream of images from videoJS during recording, in which
+        the dancer is performing. The web worker takes these images and runs
+        them through PoseNet, saving them in an array called allProcessed
+      'finished' - the dancer has finished recording! Web worker will send
+        back the allProcessed array - which should include JSON objects of all
+        processed image frames from the recorded video,
+        and the calibration object
+      ******************** */
     posenet.load(poseNetConfig.input).then(newNet => {
       net = newNet;
       postMessage({type: 'Ready'});
       self.onmessage = event => {
-        //console.log("In Worker, message received: ", event);
         if (event.data.type === 'finished') {
-          console.log('Finishing?');
           postMessage({
             type: 'All processed',
             data: allProcessed,
@@ -45,7 +63,7 @@ self.onmessage = set => {
           return;
         }
         if (event.data.type === 'calibration') {
-          console.log('calibrating');
+          // console.log('calibrating');
           net
             .estimateSinglePose(event.data.image, {
               flipHorizontal: false,
@@ -54,7 +72,7 @@ self.onmessage = set => {
             .then(result => {
               calibration = result;
             })
-            .catch(err => console.log('error while calibrating!', err));
+            .catch(err => console.error('error while calibrating!', err));
           return;
         }
         try {
@@ -68,14 +86,16 @@ self.onmessage = set => {
                 pose: result,
                 timestamp: event.data.timestamp
               });
-              // console.log(allProcessed.length);
             })
             .catch(err =>
-              console.log('Inside estimate single pose, error occurred: ', err)
+              console.error(
+                'Inside estimate single pose, error occurred: ',
+                err
+              )
             );
         } catch (err) {
-          console.log('Error in web worker: ', err);
-          console.log('event that threw this error was: ', event);
+          console.error('Error in web worker: ', err);
+          console.error('event that threw this error was: ', event);
         }
       };
     });
