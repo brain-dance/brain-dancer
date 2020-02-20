4294cb6da9e324fcce98580bf3b2cccaa29091a5
Jasmine Wang <jasminey.tea@gmail.com>
2020-02-20 11:24:51 -0600

    descriptive comment for videoJs options

--- utils/videoJsOptions.js
+++ utils/videoJsOptions.js
@@ -1,3 +1,12 @@
+/* ********************
+  This object contains the config and options needed to set up videoJS.
+  the record plugin is what we use to record videos on Coreo. We set the
+  video max length to be 10 seconds, and timeSlice to be 250ms. We need the
+  timeSlice to be set so that videoJS will fire a timestamp at that set interval,
+  and therefore afford us a way to capture the image and send it to our web worker for processing. At the time of this writing, we aren't able to get the timeSlice
+  smaller than around 250 without crashing the player...
+  ******************** */
+
 const videoJsOptions = {
   controls: false, //true,
   width: 640,
