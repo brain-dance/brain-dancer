272378647051a863e9669adc3f89db1cb18720b7
Jasmine Wang <jasminey.tea@gmail.com>
2020-02-26 15:48:45 -0600

    jsdocs

--- utils/videoProcessing.js
+++ utils/videoProcessing.js
@@ -7,18 +7,13 @@ const glob = require('glob');
 /* ********************
   This file includes functions involved in processing an uploaded video prior to
   sending it to our database.
-
-  generateFrames takes the video and passes it through ffmpeg to extract all the
-  frames, saving them in the ./utils folder.
-
-  generateWireframes takes these files, canvasifies them (using a utility
-    function, generates a temporary canvas, draws the image file, and returns),
-    and then passes the canvas through PoseNet. These results are saved in
-    sequential order in the wireframes [] array.
-
-    After the processing is done, removeFrames removes the frame jpg files that were originally generated
-    in the ./utils folder.
   ******************** */
+
+/**
+   * takes the video and passes it through ffmpeg to extract all the
+  frames, saving them in the ./utils folder.
+   * @param {string} videoPath
+   */
 const generateFrames = async videoPath => {
   await extractFrames({
     input: videoPath,
@@ -26,12 +21,24 @@ const generateFrames = async videoPath => {
   });
 };
 
+/**
+ * After the processing is done, removeFrames removes the frame jpg files that were originally generated
+    in the ./utils folder.
+ * @param {array} frameList
+ */
 const removeFrames = frameList => {
   frameList.forEach(frame => {
     fs.unlinkSync(frame);
   });
 };
 
+/**
+ * generateWireframes takes these files, canvasifies them (using a utility
+    function, generates a temporary canvas, draws the image file, and returns),
+    and then passes the canvas through PoseNet. These results are saved in
+    sequential order in the wireframes [] array.
+ * @param {string} videoPath
+ */
 const generateWireframes = async videoPath => {
   const net = await singlePoseNet();
   await generateFrames(videoPath);
